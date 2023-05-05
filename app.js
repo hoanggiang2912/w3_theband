695421dab3f756740e4b810531ded38cdce03dca
hoanggiang2912 <109599569+hoanggiang2912@users.noreply.github.com>
2023-05-05 22:06:45 +0700

    ui done

---
+++ app.js
@@ -0,0 +1,14 @@
+const slider = document.querySelector('.slider')
+
+const imgs = [
+    '/access/image/1.jpg',
+    '/access/image/2.jpg',
+    '/access/image/3.jpg',
+]
+
+function autoPlay () {
+    
+}
+
+
+
